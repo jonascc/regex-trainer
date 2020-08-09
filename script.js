4e27cb5f332718a07fb857250bdc626e182dbf46
jonascc <jonasccarmo@gmail.com>
2020-08-09 17:04:59 -0300

    Error message for invalid patterns.

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 function testString() {
     const stringToTestAgainst = document.getElementById('input_string').value;
-    // TODO check validity of pattern provided
     const patternString = document.getElementById('input_pattern').value;
-    // TODO create regex pattern from string 
-    const pattern = new RegExp(patternString, 'g');
+    try {
+        validatePattern(patternString);
+        hideErrorMessage();
+        // TODO create regex pattern from string 
+        const pattern = new RegExp(patternString, 'g');
 
-    let matchFoundElement = document.getElementById('match_found');
-    const matchFound = pattern.test(stringToTestAgainst);
-    matchFoundElement.innerHTML = matchFound;
+        // was a match found?
+        let matchFoundElement = document.getElementById('match_found');
+        const matchFound = pattern.test(stringToTestAgainst);
+        matchFoundElement.innerHTML = matchFound;
 
-    const matchesExtracted = stringToTestAgainst.match(pattern);
-    let listOfMatchesElement = document.getElementById('list_of_matches');
-    listOfMatchesElement.innerHTML = JSON.stringify(matchesExtracted);
+        // extract the maches found
+        const matchesExtracted = stringToTestAgainst.match(pattern);
+        let listOfMatchesElement = document.getElementById('list_of_matches');
+        listOfMatchesElement.innerHTML = JSON.stringify(matchesExtracted);
+    } catch (error) {
+        showErrorMessage(error);
+    }
 
+}
+
+const validatePattern = (patternString) => {
+    // verify if pattern has the format: /pattern/modifiers. Ex.: /\d+/gi
+    const validPattern = /^\/.*\/$|^\/.*\/(i{0,1}g{0,1}m{0,1})$/;
+    const isValidPattern = validPattern.test(patternString)
+    if (!isValidPattern) {
+        throw 'Invalid pattern';
+    }
+}
+
+function showErrorMessage(errMsg) {
+    toggleErrElement(errMsg);
+}
+
+function hideErrorMessage(){
+    toggleErrElement();
+}
+
+function toggleErrElement(errMsg){
+    const errorMsgElement = document.getElementById('error_msg');
+    errorMsgElement.innerHTML = errMsg || '';
+    errorMsgElement.style.display = errMsg ? 'block' : 'none';
 }
